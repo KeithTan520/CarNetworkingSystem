ee22d964fe6252d0d0f776e76cdcc3d8d386b045
linker <tanlin12@qq.com>
2023-09-12 22:18:25 +0800

    版本0.01

--- SourceDataProcess/src/main/java/cn/tencent/s3/flink/producer/FlinkKafkaWriter.java
+++ SourceDataProcess/src/main/java/cn/tencent/s3/flink/producer/FlinkKafkaWriter.java
@@ -1,5 +1,7 @@
 package cn.tencent.s3.flink.producer;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 /**
  * 文件名：FlinkKafkaWriter
  * 项目名：CarNetworkingSystem
@@ -7,10 +9,26 @@ package cn.tencent.s3.flink.producer;
  * 作者：linker
  * 创建时间：2023/9/12
  * 开发步骤：
- *
+ * todo 1.flink创建流执行环境，设置并行度
+ * todo 2.设置开启checkpoint
+ * todo 3.设置重启策略 不重启
+ * todo 4.读取File数据源，初始化 FlinkKafkaProducer及必须配置
+ * todo 5.添加数据源
+ * todo 6.写入到kafka集群
+ * todo 7.执行流环境
  */
 public class FlinkKafkaWriter {
     public static void main(String[] args) {
+        //todo 1.flink创建流执行环境，设置并行度
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        //todo 2.设置开启checkpoint
+        env.enableCheckpointing(5);
+        //todo 3.设置重启策略 不重启
 
+        //todo 4.读取File数据源，初始化 FlinkKafkaProducer及必须配置
+        //todo 5.添加数据源
+        //todo 6.写入到kafka集群
+        //todo 7.执行流环境
     }
 }
